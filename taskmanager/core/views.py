a60967979d8fe409d3b78fdc42c2d21598c2bc28
Hitanshu <hituuprajapati917@gmail.com>
2025-08-04 17:20:00 +0530

    Initial commit: Task Manager Django Project with Bootstrap UI

---
+++ taskmanager/core/views.py
@@ -0,0 +1,36 @@
+
+from django.shortcuts import render, redirect
+from .forms import Task
+from .forms import TaskForm
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'task_list.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'add_task.html', {'form': form})
+
+def edit_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'edit_task.html', {'form': form})
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('task_list')
+
+
